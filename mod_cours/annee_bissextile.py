920f1e47066c55e8dbbd928accd98c4c857b41c3
Ubunlucky <winko@live.fr>
2018-12-04 07:43:24 +0100

    Code commenté

--- mod_cours/annee_bissextile.py
+++ mod_cours/annee_bissextile.py
@@ -1,5 +1,6 @@
 """ Function testant si une année, saisie par l'utilisateur, est bissextile ou non """
 
+# Définition de la fonction
 def annee_bissextile(annee):
     if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
         print("L'année saisie est bissextile.")
